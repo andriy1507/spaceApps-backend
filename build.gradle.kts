92a8d1243ea6e41852eca0b0363ced48aa6522bd
AndriyGoryachok <andriy1507@hotmail.com>
2020-10-26 23:55:43 +0200

    Changed java version to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 group = "com.spaceapps"
 version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
