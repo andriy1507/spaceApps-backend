c71a57eb228bad9f6c7a59697cd1763567424d18
AndriyGoryachok <andriy1507@hotmail.com>
2020-10-27 00:21:18 +0200

    Added scheduled job

--- src/main/kotlin/com/spaceapps/backend/config/ScheduleConfig.kt
+++ src/main/kotlin/com/spaceapps/backend/config/ScheduleConfig.kt
@@ -1,12 +1,20 @@
 package com.spaceapps.backend.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.SchedulingConfigurer
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.scheduling.config.ScheduledTaskRegistrar
 
 @Configuration
+@EnableScheduling
 class ScheduleConfig : SchedulingConfigurer {
+
+    @Bean
+    fun taskScheduler(): ThreadPoolTaskScheduler {
+        return ThreadPoolTaskScheduler()
+    }
 
     override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
         taskRegistrar.setTaskScheduler(ThreadPoolTaskScheduler())
