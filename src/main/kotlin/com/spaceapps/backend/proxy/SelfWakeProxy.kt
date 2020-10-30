470c8f5cf19dfe20dbaa6d0f41d50d2e0c669a66
AndriyGoryachok <andriy1507@hotmail.com>
2020-10-30 11:45:56 +0200

    Updated url

--- src/main/kotlin/com/spaceapps/backend/proxy/SelfWakeProxy.kt
+++ src/main/kotlin/com/spaceapps/backend/proxy/SelfWakeProxy.kt
@@ -3,7 +3,7 @@ package com.spaceapps.backend.proxy
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient(name = "host", url = "https://develop-space-apps.herokuapp.com/")
+@FeignClient(name = "host", url = "https://develop-space-apps-backend.herokuapp.com/")
 interface SelfWakeProxy {
 
     @GetMapping("/")
