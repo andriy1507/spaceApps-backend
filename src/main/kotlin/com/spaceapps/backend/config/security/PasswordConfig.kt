a76e1a86d8a72617364335e0896ca7814340b56c
AndriyGoryachok <andriy1507@hotmail.com>
2020-11-02 16:22:41 +0200

    Changed BCrypt to SCrypt

--- src/main/kotlin/com/spaceapps/backend/config/security/PasswordConfig.kt
+++ src/main/kotlin/com/spaceapps/backend/config/security/PasswordConfig.kt
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder
 
 @Configuration
 class PasswordConfig {
 
     @Bean
-    fun passwordEncoder():PasswordEncoder{
-        return BCryptPasswordEncoder(16)
+    fun passwordEncoder(): PasswordEncoder {
+        return SCryptPasswordEncoder()
     }
 }
