0883463870ffffdce5bf471ea1f09509a5f3728d
AndriyGoryachok <andriy1507@hotmail.com>
2021-01-17 21:51:51 +0200

    Fixed multiple saving of FCM token

--- src/main/kotlin/com/spaceapps/backend/model/dao/UserDeviceDao.kt
+++ src/main/kotlin/com/spaceapps/backend/model/dao/UserDeviceDao.kt
@@ -6,10 +6,8 @@ import javax.persistence.*
 @Table
 data class UserDeviceDao(
         @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        var id: Long = 0,
-        @Column(name = "applicationUserId")
-        var userId: Long = 0,
         @Column(name = "fcmToken", unique = true)
-        var fcmToken: String = ""
+        var fcmToken: String = "",
+        @Column(name = "applicationUserId")
+        var userId: Long = 0
 )
