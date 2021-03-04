9bdb5f32939c77be0e835e7a661464d476cb95b9
AndriyGoryachok <andriy1507@hotmail.com>
2021-03-04 23:54:35 +0200

    Modified auth request models

--- src/main/kotlin/com/spaceapps/backend/controllers/AuthorizationController.kt
+++ src/main/kotlin/com/spaceapps/backend/controllers/AuthorizationController.kt
@@ -1,6 +1,7 @@
 package com.spaceapps.backend.controllers
 
 import com.spaceapps.backend.model.dao.ApplicationUser
+import com.spaceapps.backend.model.dto.AuthRequestDto
 import com.spaceapps.backend.model.exceptions.UsernameExistsException
 import com.spaceapps.backend.repositories.ApplicationUserRepository
 import com.spaceapps.backend.services.AuthorizationService
@@ -31,15 +32,12 @@ class AuthorizationController constructor(
 //        }
 //    }
 
-    @GetMapping("/sign-in")
+    @PostMapping("/sign-in")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation("Returns access and refresh tokens")
-    fun singInWithUserNameAndPassword(
-            @RequestParam("userName") userName: String,
-            @RequestParam("password") password: String
-    ): ResponseEntity<*> {
+    fun singInWithUserNameAndPassword(@RequestBody request:AuthRequestDto): ResponseEntity<*> {
         return try {
-            ResponseEntity.ok(authorizationService.signInUserNamePassword(userName, password))
+            ResponseEntity.ok(authorizationService.signInUserNamePassword(request.email, request.password))
         } catch (e: Exception) {
             ResponseEntity.badRequest().body(e)
         }
@@ -48,12 +46,9 @@ class AuthorizationController constructor(
     @PostMapping("/sign-up")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation("Creates new user and returns access and refresh tokens")
-    fun signUpWithUserNameAndPassword(
-            @RequestParam("userName") userName: String,
-            @RequestParam("password") password: String
-    ): ResponseEntity<*> {
+    fun signUpWithUserNameAndPassword(@RequestBody request:AuthRequestDto): ResponseEntity<*> {
         return try {
-            val token = authorizationService.signUpUserNamePassword(userName, password)
+            val token = authorizationService.signUpUserNamePassword(request.email, request.password)
             ResponseEntity.ok(token)
         } catch (e: UsernameExistsException) {
             ResponseEntity.badRequest().body(e.localizedMessage)
---
+++ src/main/kotlin/com/spaceapps/backend/model/dto/AuthRequestDto.kt
@@ -0,0 +1,26 @@
+package com.spaceapps.backend.model.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class AuthRequestDto(
+        @SerializedName("email")
+        val email: String,
+        @SerializedName("password")
+        val password: String,
+        @SerializedName("device")
+        val device: UserDeviceDto
+) {
+    data class UserDeviceDto(
+            @SerializedName("token")
+            val token: String,
+            @SerializedName("platform")
+            val platform: Platform
+    ) {
+        enum class Platform {
+            @SerializedName("android")
+            Android,
+            @SerializedName("ios")
+            Ios
+        }
+    }
+}
