4a579fb8cecbbcb0ec8d94ce5ad62dc5eca4ffb7
AndriyGoryachok <andriy1507@hotmail.com>
2021-03-17 14:22:10 +0200

    Fixed static content

--- src/main/kotlin/com/spaceapps/backend/controller/StaticController.kt
+++ src/main/kotlin/com/spaceapps/backend/controller/StaticController.kt
@@ -3,11 +3,13 @@ package com.spaceapps.backend.controller
 import com.spaceapps.backend.model.dto.static.StaticContentResponse
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiImplicitParam
+import org.springframework.core.io.ClassPathResource
 import org.springframework.util.ResourceUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.BufferedInputStream
 
 @Api(value = "Static", tags = ["Static"], description = "Static content endpoints")
 @RestController
@@ -25,7 +27,7 @@ class StaticController {
     fun getStaticContent(
         @PathVariable("type") type: String
     ): StaticContentResponse {
-        val content = ResourceUtils.getFile("classpath:$type").readText()
+        val content = BufferedInputStream(ClassPathResource(type).inputStream).reader().readText()
         return StaticContentResponse(content)
     }
 }
