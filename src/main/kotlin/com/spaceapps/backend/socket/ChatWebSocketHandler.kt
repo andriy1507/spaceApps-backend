b00b15aa86f63b7bfe69a433e7db14054510c261
AndriyGoryachok <andriy1507@hotmail.com>
2021-03-19 23:02:57 +0200

    Changed web-socket

--- src/main/kotlin/com/spaceapps/backend/socket/ChatWebSocketHandler.kt
+++ src/main/kotlin/com/spaceapps/backend/socket/ChatWebSocketHandler.kt
@@ -4,6 +4,7 @@ import com.spaceapps.backend.service.ChatService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.PongMessage
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.AbstractWebSocketHandler
@@ -12,6 +13,10 @@ import org.springframework.web.socket.handler.AbstractWebSocketHandler
 class ChatWebSocketHandler @Autowired constructor(
     private val chatService: ChatService
 ) : AbstractWebSocketHandler() {
+    override fun handlePongMessage(session: WebSocketSession, message: PongMessage) {
+        session.sendMessage(PongMessage())
+        super.handlePongMessage(session, message)
+    }
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
         session.sendMessage(TextMessage("CONNECTED"))
