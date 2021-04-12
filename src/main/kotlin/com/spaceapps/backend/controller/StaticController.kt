c76b33b48c7f34f3e0f416eddf614b281b320f2a
AndriyGoryachok <andriy1507@hotmail.com>
2021-04-12 04:37:03 +0300

    Some changes

--- src/main/kotlin/com/spaceapps/backend/controller/StaticController.kt
+++ src/main/kotlin/com/spaceapps/backend/controller/StaticController.kt
@@ -2,15 +2,12 @@ package com.spaceapps.backend.controller
 
 import com.spaceapps.backend.model.dto.static.StaticContentResponse
 import io.swagger.annotations.Api
-import io.swagger.annotations.ApiImplicitParam
 import io.swagger.annotations.ApiOperation
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.ModelAndView
 import java.io.BufferedInputStream
 
 @Api(value = "Static", tags = ["Static"], description = "Static endpoints")
@@ -21,7 +18,7 @@ class StaticController {
     @GetMapping("/terms-of-use")
     @ApiOperation("Returns web-page with Terms of use", response = StaticContentResponse::class)
     fun getTermsOfUse(): ResponseEntity<StaticContentResponse> {
-        val resource = ClassPathResource("/templates/terms-of-use.html")
+        val resource = ClassPathResource("/static/terms-of-use.html")
         val content = BufferedInputStream(resource.inputStream).reader().readText()
         return ResponseEntity.ok(StaticContentResponse(content))
     }
@@ -29,7 +26,7 @@ class StaticController {
     @GetMapping("/privacy-policy")
     @ApiOperation("Returns web-page with Privacy policy", response = StaticContentResponse::class)
     fun getStaticContent(): ResponseEntity<StaticContentResponse> {
-        val resource = ClassPathResource("/templates/privacy-policy.html")
+        val resource = ClassPathResource("/static/privacy-policy.html")
         val content = BufferedInputStream(resource.inputStream).reader().readText()
         return ResponseEntity.ok(StaticContentResponse(content))
     }
