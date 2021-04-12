5ef82cfaa48af0b71337cf57638ab70ee0402ce9
AndriyGoryachok <andriy1507@hotmail.com>
2021-04-12 04:13:49 +0300

    Some changes

---
+++ src/main/kotlin/com/spaceapps/backend/controller/SettingsController.kt
@@ -0,0 +1,35 @@
+package com.spaceapps.backend.controller
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiOperation
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.annotations.ApiIgnore
+
+@RestController
+@RequestMapping("settings")
+@Api(tags = ["Settings"], description = "Settings endpoints")
+class SettingsController {
+
+    @ApiOperation("Returns paginated notifications")
+    @ApiImplicitParam(
+        name = "Authorization",
+        value = "Access token",
+        paramType = "header",
+        dataTypeClass = String::class,
+        required = true
+    )
+    @PutMapping("/set-language/{language}")
+    fun setUserLanguage(
+        @PathVariable("language") language: String,
+        @ApiIgnore auth: Authentication
+    ): ResponseEntity<*> {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null)
+    }
+}
--- src/main/kotlin/com/spaceapps/backend/controller/StaticController.kt
+++ src/main/kotlin/com/spaceapps/backend/controller/StaticController.kt
@@ -5,12 +5,12 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiImplicitParam
 import io.swagger.annotations.ApiOperation
 import org.springframework.core.io.ClassPathResource
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 import java.io.BufferedInputStream
 
 @Api(value = "Static", tags = ["Static"], description = "Static endpoints")
@@ -18,24 +18,19 @@ import java.io.BufferedInputStream
 @RequestMapping("static")
 class StaticController {
 
-    @GetMapping("/{type}")
-    @ApiOperation("Returns static content of given type", response = StaticContentResponse::class)
-    @ApiImplicitParam(
-        name = "type",
-        paramType = "path",
-        allowableValues = "terms-of-use, privacy-policy",
-        allowEmptyValue = false,
-        required = true
-    )
-    fun getStaticContent(
-        @PathVariable("type") type: String
-    ): ResponseEntity<*> {
-        val resource = ClassPathResource("/static/$type")
-        return if (resource.exists()) {
-            val content = BufferedInputStream(resource.inputStream).reader().readText()
-            ResponseEntity.ok(StaticContentResponse(content))
-        } else {
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
-        }
+    @GetMapping("/terms-of-use")
+    @ApiOperation("Returns web-page with Terms of use", response = StaticContentResponse::class)
+    fun getTermsOfUse(): ResponseEntity<StaticContentResponse> {
+        val resource = ClassPathResource("/templates/terms-of-use.html")
+        val content = BufferedInputStream(resource.inputStream).reader().readText()
+        return ResponseEntity.ok(StaticContentResponse(content))
+    }
+
+    @GetMapping("/privacy-policy")
+    @ApiOperation("Returns web-page with Privacy policy", response = StaticContentResponse::class)
+    fun getStaticContent(): ResponseEntity<StaticContentResponse> {
+        val resource = ClassPathResource("/templates/privacy-policy.html")
+        val content = BufferedInputStream(resource.inputStream).reader().readText()
+        return ResponseEntity.ok(StaticContentResponse(content))
     }
 }
